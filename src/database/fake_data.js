189ab928081e251793be66ff386ae4a25b695bca
amanda.almeida <amandagundes@outlook.com>
2020-08-08 21:57:26 -0300

    Update

--- src/database/fake_data.js
+++ src/database/fake_data.js
@@ -11,7 +11,7 @@ const proffys = [
         time_to: [1220] 
     },
     {
-        name: "Amanda Almeida",
+        name: "Amanda Gundes",
         avatar: "https://avatars2.githubusercontent.com/u/55407153?s=460&u=019141b7eac59d72362f9bdedf4d5d67d5f2a1da&v=4",
         whataspp: "48991271266",
         bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
